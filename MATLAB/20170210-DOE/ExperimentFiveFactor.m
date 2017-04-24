7a72fd1425beffd7250862e164fd618650190506
Suresh Kumar Gadi <gadisureshkumar@gmail.com>
2017-04-23 23:04:29 -0500

    Automatic push

--- MATLAB/20170210-DOE/ExperimentFiveFactor.m
+++ MATLAB/20170210-DOE/ExperimentFiveFactor.m
@@ -3,6 +3,8 @@ function [ Y ] = ExperimentFiveFactor( Start, End, maxRange, x )
 %   Detailed explanation goes here
 Period = End - Start;
 maxPoint = Start + Period*(maxRange)*rand() + Period*(1-maxRange)/2;
-Y = 4.*exp(3/Period.*(-x+maxPoint))./((1+exp(3/Period.*(-x+maxPoint))).^2);
+% Y = 4.*exp(3/Period.*(-x+maxPoint))./((1+exp(3/Period.*(-x+maxPoint))).^2);
+Y = 1./(1+exp(3/Period.*(-x+maxPoint)));
+Y = 4*Y.*(1-Y);
 end
 
