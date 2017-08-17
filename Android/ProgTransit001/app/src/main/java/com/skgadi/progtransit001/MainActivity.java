f2ce5650a09908cc5db76c92664d99c3f0a724af
Suresh Kumar Gadi <gadisureshkumar@gmail.com>
2017-08-16 19:51:21 -0500

    Automatic push

--- Android/ProgTransit001/app/src/main/java/com/skgadi/progtransit001/MainActivity.java
+++ Android/ProgTransit001/app/src/main/java/com/skgadi/progtransit001/MainActivity.java
@@ -1,6 +1,10 @@
 package com.skgadi.progtransit001;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Build;
 import android.os.Environment;
 import android.support.annotation.StringRes;
 import android.support.v7.app.AppCompatActivity;
@@ -11,28 +15,33 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.io.File;
-import java.io.FileOutputStream;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private File folder;
-    private LinearLayout MV_FilesList;
-    private LinearLayout MV_AssignInputs;
+    private LinearLayout MV_Databases;
+    private LinearLayout MV_States;
     private RadioGroup FL_ProfilesList;
     private Button FL_ProfileSelectedBtn;
     private EditText FL_NewProfileEdtBox;
+    private Button FL_DeleteProfileBtn;
+
+    Spinner SV_CycleSelect;
+
+
+    private Context context;
 
     //Defined By SKGadi
     enum GLOBAL_DEVICE_STATE {
         NONE,
         FILES_LIST,
-        INPUTS,
-        CYCLE_TYPES,
         STATES,
+        CYCLE_TYPES,
+        EVENTS,
         PROGRAM
     }
 
@@ -41,125 +50,129 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //SKGadi edit
-        MV_FilesList = (LinearLayout) findViewById(R.id.MV_FilesList);
-        MV_AssignInputs = (LinearLayout) findViewById(R.id.MV_AssignInputs);
+        //Added by SKGadi ---- Initialization
+        Context context = this;
+        MV_Databases = (LinearLayout) findViewById(R.id.MV_Databases);
+        MV_States = (LinearLayout) findViewById(R.id.MV_States);
         FL_ProfilesList = (RadioGroup) findViewById(R.id.FL_ProfilesList);
         FL_ProfileSelectedBtn = (Button) findViewById(R.id.FL_ProfileSelectedBtn);
         FL_NewProfileEdtBox = (EditText) findViewById(R.id.FL_NewProfileEdtBox);
-        //Initializing activities
-        if (GetFolderHandle().exists())
-            GLOBAL_SetViewState(GLOBAL_DEVICE_STATE.FILES_LIST);
-        else {
-            GLOBAL_SetViewState(GLOBAL_DEVICE_STATE.NONE);
-            return;
-        }
+        FL_DeleteProfileBtn =  (Button) findViewById(R.id.FL_DeleteProfileBtn);
+
+        Spinner SV_CycleSelect = (Spinner) findViewById(R.id.SV_CycleSelect);
+        GLOBAL_SetViewState(GLOBAL_DEVICE_STATE.FILES_LIST);
         ListAllFilesList();
     }
-    public void FL_OC_RefreshList (View v) {
-        ListAllFilesList ();
+    /*----- Global view functions -----*/
+    private void GLOBAL_SetViewState (GLOBAL_DEVICE_STATE state) {
+        GLOBAL_SetInitialCondition();
+        switch (state) {
+            case FILES_LIST:
+                MV_Databases.setVisibility(View.VISIBLE);
+                return;
+            case STATES:
+                MV_States.setVisibility(View.VISIBLE);
+                return;
+            default:
+                return;
+        }
     }
-
+    private void GLOBAL_SetInitialCondition() {
+        MV_Databases.setVisibility(View.GONE);
+        MV_States.setVisibility(View.GONE);
+        FL_ProfileSelectedBtn.setEnabled(false);
+        FL_ProfilesList.clearCheck();
+    }
+    /*----- MV_Databases Functions -----*/
     private void ListAllFilesList () {
+        FL_DeleteProfileBtn.setEnabled(false);
         FL_ProfileSelectedBtn.setEnabled(false);
         FL_ProfilesList.removeAllViews();
-        File List[] = folder.listFiles();
         RadioButton RadioItem;
-        for (int i=0; i<List.length; i++) {
-            if (List[i].length() < (3*1024)) {
+        String Databases[] = getApplicationContext().databaseList();
+        for (int i=0; i<Databases.length; i++) {
+            if (!Databases[i].matches(".+-journal")) {
                 RadioItem = new RadioButton(this);
-                RadioItem.setText(List[i].getName());
+                RadioItem.setText(Databases[i]);
                 RadioItem.setOnClickListener(new View.OnClickListener() {
                     public void onClick(View v) {
                         FL_ProfileSelectedBtn.setEnabled(true);
+                        FL_DeleteProfileBtn.setEnabled(true);
                     }
                 });
                 FL_ProfilesList.addView(RadioItem);
             }
         }
     }
-
-    private boolean CreateNewFileWithZeros() {
-        File file = new File(folder, FL_NewProfileEdtBox.getText().toString());
-        if (!file.exists()) {
-            String string = "\0";
-            FileOutputStream outputStream;
-            try {
-                outputStream = new FileOutputStream(file);
-                outputStream.write(string.getBytes());
-                outputStream.close();
-                return true;
-            } catch (Exception e) {
-                Toast.makeText(getApplicationContext(), "Unable to create the file", Toast.LENGTH_SHORT).show();
-                e.printStackTrace();
-                return false;
-            }
-        } else
-            Toast.makeText(getApplicationContext(), R.string.error_FileAlreadyExist, Toast.LENGTH_SHORT).show();
-        return false;
-    }
-    private File GetFolderHandle () {
-        folder = new File(getApplicationContext().getExternalFilesDir(
-                null), "profiles");
-        if (folder.exists())
-            Toast.makeText(getApplicationContext(),"Folder found", Toast.LENGTH_SHORT).show();
-        else {
-            Toast.makeText(getApplicationContext(),"Folder not found", Toast.LENGTH_SHORT).show();
-            if (!folder.mkdirs()) {
-                Toast.makeText(getApplicationContext(), R.string.error_UnableToCreateFolder, Toast.LENGTH_LONG).show();
-            }
-        }
-        return folder;
-    }
-
-    private void GLOBAL_SetViewState (GLOBAL_DEVICE_STATE state) {
-        GLOBAL_SetInitialCondition();
-        switch (state) {
-            case FILES_LIST:
-                MV_FilesList.setVisibility(View.VISIBLE);
-                return;
-            case INPUTS:
-                MV_AssignInputs.setVisibility(View.VISIBLE);
-                return;
-            default:
-                return;
-        }
-    }
-
-    private void GLOBAL_SetInitialCondition() {
-        MV_FilesList.setVisibility(View.GONE);
-        MV_AssignInputs.setVisibility(View.GONE);
-        FL_ProfileSelectedBtn.setEnabled(false);
-        FL_ProfilesList.clearCheck();
-    }
-
     public void FL_OC_AddNewProfile (View v) {
-        if (CreateNewFileWithZeros())
-            ListAllFilesList();
+        String DatabaseName = FL_NewProfileEdtBox.getText().toString();
+        if (doesDatabaseExist(getApplicationContext(),"GSK_"+DatabaseName))
+            Toast.makeText(getApplicationContext(), getResources().getString(R.string.error_DatabaseAlreadyExist), Toast.LENGTH_SHORT).show();
+        else{
+            SQLiteDatabase NewDatabase = getApplicationContext().openOrCreateDatabase("GSK_" + DatabaseName, MODE_PRIVATE, null);
+            CreateTables(getApplicationContext(), NewDatabase);
+            NewDatabase.close();
+        }
+        ListAllFilesList();
+    }
+    private void CreateTables (Context Context, SQLiteDatabase Database) {
+        Database.execSQL("CREATE TABLE IF NOT EXISTS `States` (" +
+                "`CycleType`    int(3) NOT NULL," +
+                "`Period`       int(3) NOT NULL," +
+                "`D00`          int(1) NOT NULL," +
+                "`D01`          int(1) NOT NULL," +
+                "`D02`          int(1) NOT NULL," +
+                "`D03`          int(1) NOT NULL," +
+                "`D04`          int(1) NOT NULL," +
+                "`D05`          int(1) NOT NULL," +
+                "`D06`          int(1) NOT NULL," +
+                "`D07`          int(1) NOT NULL," +
+                "`D08`          int(1) NOT NULL," +
+                "`D09`          int(1) NOT NULL," +
+                "`D10`          int(1) NOT NULL," +
+                "`D11`          int(1) NOT NULL," +
+                "`Audio`        int(1) NOT NULL" +
+                ");");
+        Database.execSQL("CREATE TABLE IF NOT EXISTS `Events` (" +
+                "`EventID`      int(3) NOT NULL," +
+                "`StartTime`    DATETIME NOT NULL," +
+                "`EndTime`      DATETIME NOT NULL," +
+                "`CycleType`    int(3) NOT NULL," +
+                "`Sunday`       int(1) NOT NULL," +
+                "`Monday`       int(1) NOT NULL," +
+                "`Tuesday`      int(1) NOT NULL," +
+                "`Wednesday`    int(1) NOT NULL," +
+                "`Thursday`     int(1) NOT NULL," +
+                "`Friday`       int(1) NOT NULL," +
+                "`Saturday`     int(1) NOT NULL," +
+                "PRIMARY KEY (`EventID`)"+
+                ");");
+        Database.execSQL("CREATE TABLE IF NOT EXISTS `Settings` (" +
+                "`Attribute`    VARCHAR(20) NOT NULL," +
+                "`Value`        VARCHAR(20) NOT NULL," +
+                "PRIMARY KEY (`Attribute`)"+
+                ");");
+    }
+    public void FL_OC_DeleteProfile (View v) {
+        String DatabaseToDelete =
+                ((RadioButton) findViewById(FL_ProfilesList.getCheckedRadioButtonId())).getText().toString();
+        //Toast.makeText(getApplicationContext(), DatabaseToDelete, Toast.LENGTH_SHORT).show();
+        getApplicationContext().deleteDatabase(DatabaseToDelete);
+        ListAllFilesList();
+    }
+    public void FL_OC_RefreshList (View v) {
+        ListAllFilesList ();
     }
     public void FL_OC_ProfileSelected (View v) {
-        GLOBAL_SetViewState(GLOBAL_DEVICE_STATE.INPUTS);
+        GLOBAL_SetViewState(GLOBAL_DEVICE_STATE.STATES);
     }
-    public void TempButton (View v) {
+    /*----- MV_States Functions -----*/
+    public void BackToFilesList (View v) {
         GLOBAL_SetViewState(GLOBAL_DEVICE_STATE.FILES_LIST);
     }
-    /*------------------- Taken from 3rd party -------------------*/
-    /* Checks if external storage is available for read and write */
-    public boolean isExternalStorageWritable() {
-        String state = Environment.getExternalStorageState();
-        if (Environment.MEDIA_MOUNTED.equals(state)) {
-            return true;
-        }
-        return false;
-    }
-
-    /* Checks if external storage is available to at least read */
-    public boolean isExternalStorageReadable() {
-        String state = Environment.getExternalStorageState();
-        if (Environment.MEDIA_MOUNTED.equals(state) ||
-                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
-            return true;
-        }
-        return false;
+    /*----- General Functions -----*/
+    private static boolean doesDatabaseExist(Context context, String dbName) {
+        File dbFile = context.getDatabasePath(dbName);
+        return dbFile.exists();
     }
 }
